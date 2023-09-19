ffcab97c5f5db71c0a957c0ad14de912c00c4aa6
Djumabaevs <djumabaevb@gmail.com>
2023-09-19 11:27:49 +0600

    average2

--- lib/algos/algos.dart
+++ lib/algos/algos.dart
@@ -453,3 +453,21 @@ class Solution14 {
     return maxAvg;
   }
 }
+
+class Solution14b {
+  double findMaxAverage(List<int> nums, int k) {
+    int maxSum = 0;
+    int sum = 0;
+    for (int i = 0; i < k; i++) {
+      sum += nums[i];
+    }
+    maxSum = sum;
+    for (int i = k; i < nums.length; i++) {
+      sum += nums[i] - nums[i - k];
+      if (sum > maxSum) {
+        maxSum = sum;
+      }
+    }
+    return maxSum / k;
+  }
+}
