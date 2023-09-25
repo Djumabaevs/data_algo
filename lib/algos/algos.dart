13e0c9e4b41ed5b2647b1ae4dd2839d334a8b384
Djumabaevs <djumabaevb@gmail.com>
2023-09-25 16:26:19 +0600

    duplicate

--- lib/algos/algos.dart
+++ lib/algos/algos.dart
@@ -471,3 +471,22 @@ class Solution14b {
     return maxSum / k;
   }
 }
+
+class Solution15 {
+  List<int> findErrorNums(List<int> nums) {
+    int n = nums.length;
+    int sum = (n * (n + 1) ~/ 2);
+    int duplicate = -1, missing = -1;
+
+    Set<int> set = {};
+    for (int i = 0; i < n; i++) {
+      if (!set.add(nums[i])) {
+        duplicate = nums[i];
+      }
+      sum -= nums[i];
+      print('SUM: $sum');
+    }
+    missing = sum + duplicate;
+    return [duplicate, missing];
+  }
+}
