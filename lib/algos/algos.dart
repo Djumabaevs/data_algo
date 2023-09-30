67cfd055e40905f75a52eec463e034318a5b6b02
Djumabaevs <djumabaevb@gmail.com>
2023-10-01 01:23:32 +0600

    ml

--- lib/algos/algos.dart
+++ lib/algos/algos.dart
@@ -529,3 +529,41 @@ class MyStack {
     return queue1.isEmpty && queue2.isEmpty;
   }
 }
+
+class MyStack2 {
+  late Queue<int> q;
+  MyStack() {
+    q = Queue<int>();
+  }
+
+  void push(int x) {
+    q.add(x);
+  }
+
+  int pop() {
+    int sz = q.length;
+    if (sz == 0) return 0;
+    for (int i = 0; i < sz - 1; i++) {
+      int val = q.removeFirst();
+      q.add(val);
+    }
+    int val = q.removeFirst();
+    return val;
+  }
+
+  int top() {
+    int sz = q.length;
+    if (sz == 0) return 0;
+    for (int i = 0; i < sz - 1; i++) {
+      int val = q.removeFirst();
+      q.add(val);
+    }
+    int val = q.removeFirst();
+    q.add(val);
+    return val;
+  }
+
+  bool empty() {
+    return q.isEmpty;
+  }
+}
